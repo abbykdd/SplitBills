72f6a5a6c9463cfe50fdc18a2bf306a18ea3198d
Ayhan <ayqolpan.kdd@gmail.com>
2017-08-31 16:20:24 -0700

    can open the second view

--- SplitBills/InputBillsViewController.swift
+++ SplitBills/InputBillsViewController.swift
@@ -1,26 +1,25 @@
 //
-//  HomePageViewController.swift
+//  InputBillsViewController.swift
 //  SplitBills
 //
-//  Created by Ayhan on 2017-08-19.
+//  Created by Ayhan on 2017-08-29.
 //  Copyright © 2017 Ayhan. All rights reserved.
 //
 
 import UIKit
 
-class HomePageViewController: UIViewController {
-  
+class InputBillsViewController: UIViewController {
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
-
---
+++ SplitBills/NameBillViewController.swift
@@ -0,0 +1,33 @@
+//
+//  NameBillViewController.swift
+//  SplitBills
+//
+//  Created by Ayhan on 2017-08-29.
+//  Copyright © 2017 Ayhan. All rights reserved.
+//
+
+import UIKit
+
+class NameBillViewController: UIViewController {
+    
+//    @IBOutlet weak var nameOfBill: UILabel!
+//    
+//    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        
+    }
+//
+//    override func viewWillDisappear(_ animated: Bool) {
+//        super.viewWillDisappear(animated);
+//        
+//        let billName : String = nameOfBill.text!;
+//        print(billName);
+//    }
+    
+}
